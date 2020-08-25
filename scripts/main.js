36c43a25081b1c8d03074322125fe964050e9598
Michael Khanna <mikekhanna@icloud.com>
2020-08-25 16:18:58 -0700

    Changed Small Visual Details

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,12 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  let myName = prompt('Please enter your name.');
+  let myName = prompt('Please Enter Complete');
   if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Test is complete, ' + myName;
+    myHeading.innerHTML = 'Test is ' + myName;
   }
 }
 
@@ -15,7 +15,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Test Prompt Is Complete, ' + storedName;
+  myHeading.textContent = 'Test Prompt Is Not Complete'
 }
 
 myButton.onclick = function() {
